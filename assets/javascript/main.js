48967701a0c9f3b8a33a37128b7fdde55dca50ac
jmyager <jmyager6@gmail.com>
2018-06-23 17:27:07 -0400

    update 2.1

---
+++ assets/javascript/main.js
@@ -0,0 +1,47 @@
+$("#portfolio", "#about").hide();
+
+$("a").on("click", function () {
+    $("#portfolio", "#about").show();
+})
+
+// Scroll on anchor click
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('#scrollBtn').fadeIn();
+        } else {
+            $('#scrollBtn').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('#scrollBtn').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
